7d662d69bf3f2803ce01f60f1a3ef92542ca9cf6
matt0x20 <pxmatija@gmail.com>
2026-07-23 12:49:49 +0200

    smart gaps

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -370,3 +370,11 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, rounding = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, rounding = 0 })
