7852583adc4c46b6eb67aab99c0ad82a90cef7b2
matt0x20 <pxmatija@gmail.com>
2026-07-23 22:28:52 +0200

    fix errors

--- modules/user/sources.nix
+++ modules/user/sources.nix
@@ -1,12 +1,12 @@
 { config, pkgs, inputs, ... }:
 
 {
-  home.file.".icons".source = ./.icons;
-  home.file.".config/hypr".source = ./.config/hypr;
-  home.file.".config/noctalia".source = ./.config/noctalia;
-  home.file.".config/kdeglobals".source = ./.config/kdeglobals;
-  home.file.".config/dolphinrc".source = ./.config/dolphinrc;
-  home.file.".config/kitty/kitty.conf".source = ./.config/kitty/kitty.conf;
-  home.file.".config/kitty/scroll_mark.py".source = ./.config/kitty/scroll_mark.py;
-  home.file.".config/kitty/search.py".source = ./.config/kitty/search.py;
+  home.file.".icons".source = ../../.icons;
+  home.file.".config/hypr".source = ../../.config/hypr;
+  home.file.".config/noctalia".source = ../../.config/noctalia;
+  home.file.".config/kdeglobals".source = ../../.config/kdeglobals;
+  home.file.".config/dolphinrc".source = ../../.config/dolphinrc;
+  home.file.".config/kitty/kitty.conf".source = ../../.config/kitty/kitty.conf;
+  home.file.".config/kitty/scroll_mark.py".source = ../../.config/kitty/scroll_mark.py;
+  home.file.".config/kitty/search.py".source = ../../.config/kitty/search.py;
 }
