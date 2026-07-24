b3e83e300980214213ff55dd3d226cce0d45fe8d
matt0x20 <pxmatija@gmail.com>
2026-07-24 23:21:58 +0200

    dfssdfsfd

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -14,10 +14,10 @@
   boot = {
     plymouth = {
       enable = true;
-      theme = "rings";
+      theme = "spinner";
       themePackages = with pkgs; [
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
+          selected_themes = [ "spinner" ];
         })
       ];
     };
