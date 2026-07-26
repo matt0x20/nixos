e4a33e0d46d11b4a61525d90e82c734e2289f1b7
matt0x20 <pxmatija@gmail.com>
2026-07-26 22:54:49 +0200

    fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,6 +4,7 @@
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     noto-fonts-color-emoji
     liberation_ttf
     fira-code
@@ -13,4 +14,10 @@
     proggyfonts
     adwaita-fonts
   ];
+
+  fontconfig.defaultFonts = {
+    sansSerif = [ "Noto Sans CJK JP" "Adwaita Sans" ];
+    serif = [ "Noto Serif CJK JP" "DejaVu Serif" ];
+    monospace = [ "Noto Sans Mono CJK JP" "Adwaita Mono" ];
+  };
 }
